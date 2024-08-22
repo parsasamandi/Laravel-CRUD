70f8566e56aa28776c2f9d9002f18d4498f25369
Parsa Samandi <parsa_samandi@Pouyas-MacBook-Air.local>
2024-08-22 16:26:24 -0700

    Optimize the RequestHandler.js

--- resources/assets/js/RequestHandler.js
+++ resources/assets/js/RequestHandler.js
@@ -1,3 +1,4 @@
+// Efficient process of handling ajax requests
 class RequestHandler {
     // Constructor
     constructor(dt,formId,url) {
@@ -6,23 +7,32 @@ class RequestHandler {
         window.url = url; // Url
     }
 
-    // modal
-    openModal() {
-        $('#formModal').modal('show');
+    // Modal
+    openInsertionModal() {
+        // Empty the form output
+        emptyFormOutput();
+        // Show the insertion/update modal
+        showFormModal();
+        // Set the button action for insertion
         $('#button_action').val('insert');
-        $('#action').val('Submit');
-        $('#form_output').html('');
+        // Set the button value to 'تایید'
+        $('#action').val('تایید');
+        // Set the sign * as 'required'
+        $('.required-heading .input-required').show();
+        // Set the selectbox values to null
+        $('select').val(null).trigger('change');
+        // Reset the dataTable
         $(window.formId)[0].reset();
+        window.dt.cear().draw();
     }
 
-    // Insert
+    // Insertion
     insert() {
         // Store or Update
         $(window.formId).on('submit', function (event) {
             event.preventDefault();
             // Form Data
-            var form_data = new FormData(this);
-            form_data.append('file', form_data);
+            const form_data = new FormData(event.target); // Include all form data
 
             $.ajax({
                 url: "/" + window.url + "/store",
@@ -32,11 +42,10 @@ class RequestHandler {
                 cache: false,
                 data: form_data,
                 success: function (data) { 
-                    success(data);
+                    success(data, "#formModal");
                 },
                 error: function (data) {
-                    if(data)
-                        error(data);
+                    error(data);
                 }
             })
         });
@@ -44,55 +53,87 @@ class RequestHandler {
 
     // Delete
     delete(id) {
-        $('#confirmationModal').modal('show'); // Confirmation
-        $('#deleteSubmission').click(function () {
+        $('#confirmationModal').modal('show'); // Confirm
+
+        $('#ok_button').click(function () {
             $.ajax({
-                url: "/" + window.url + "/delete/" + id,
+                url: "/" + window.url + "/delete",
                 method: "get",
+                data: { id: id },
                 success: function(data) {
-                    $('#confirmationModal').modal('hide');
-                    window.dt.draw(false);
+                    success(data, "#confirmationModal");
+                },
+                error: function (data) {
+                    error(data);
                 }
             })
         });
     }
 
-    // Reload form modal
+    // Default edit data
     reloadModal() {
-        $('#form_output').html('');
-        $('#formModal').modal('show');
-    }
-
-    // Cleaning dropbox
-    cleanDropbox(name) {
-        $(name).val('').trigger('change');
+        // Empty the form output
+        emptyFormOutput();
+        // Show the #formModal
+        showFormModal();
     }
 
     // Edit on success
     editOnSuccess(id) {
         $('#id').val(id);
+        // Set the button action for update
         $('#button_action').val('update');
-        $('#action').val('Edit');
+        // Set the button value to 'ثبت تغییرات'
+        $('#action').val('ثبت تغییرات');
+        // Remove "required field" from heading
+        $('.required-heading .input-required').hide();
     }
 }
 
-// Success
-function success(data) {
-    $('#form_output').html(data.message);
+// Show the form modal
+function showFormModal() {
+    $('#formModal').modal('show');
+}
+
+// Empty the form output
+function emptyFormOutput() {
+    $('.form_output').empty();
+}
+
+// Success handler
+function success(data, modal) {
+    // Hide the current modal
+    $(modal).modal('hide');
+    // Show the success modal if the modal was #formModal
+    modal === "#formModal" && $('#successModal').modal('show');
+    // Empty the form output
+    emptyFormOutput();
+    // Reset the form
     $(window.formId)[0].reset();
-    if(window.dt != null) {
-        window.dt.draw(false);
-    }
+    // Check if dataTable is not null, then refresh
+    window.dt && window.dt.draw(false);
 }
 
-// Error
+// Error Handler
 function error(data) {
-    // Parse To Json
-    var data = JSON.parse(data.responseText);
-    // Error
-    error_html = '';
-    for(var all in data.errors) {
-        error_html += '<div class="alert alert-danger">' + data.errors[all] + '</div>';
+    // Parse the JSON response
+    const response = JSON.parse(data.responseText);
+
+    // Initialize error HTML
+    let error_html = '';
+
+    // Add field-specific errors if they exist
+    if (response.errors) {
+        Object.values(response.errors).forEach(errors => {
+            error_html += errors.map(error => `<div class="alert alert-danger">${error}</div>`).join('');
+        });
     }
-    $('#form_output').html(error_html);
+
+    // If there are no field-specific errors, show the response.message
+    if (!error_html && response.message) {
+        error_html = `<div class="alert alert-danger">${response.message}</div>`;
+    }
+
+    // Display all errors in the form_output element
+    $('.form_output').html(error_html);
 }
